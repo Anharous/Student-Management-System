47d24e38b2a754eb891044aad72d96459234ae9a
Anharous <amirtha.0726@gmail.com>
2025-12-05 15:27:58 +0530

    feat:env file

--- src/main/java/com/example/util/DBConnection.java
+++ src/main/java/com/example/util/DBConnection.java
@@ -2,11 +2,14 @@ package com.example.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DBConnection {
+    private static final Dotenv dotenv = Dotenv.load();
 
     private final static String URL="jdbc:mysql://localhost:3306/student";
     private final static String USER="root";
-    private final static String PWD="Amir26";
+    private final static String PWD=dotenv.get("DB_PASSWORD");
     public static Connection getConnection(){
         Connection con=null;
         try{
